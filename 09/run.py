3cea58705acab8994d19531f273805f7a7af5bea
Povilas Balzaravicius Pawka <pavvka@gmail.com>
2020-12-09 09:10:20 +0200

    09: done

---
+++ 09/run.py
@@ -0,0 +1,50 @@
+import array
+
+LENGTH = 25
+
+
+def read(filename):
+    data = []
+    with open(filename, 'r') as reader:
+        for line in reader.readlines():
+            data.append(int(line.strip()))
+    return data
+
+
+def solve(numbers):
+    preamble = array.array('Q', numbers[:LENGTH])
+
+    for number in numbers[LENGTH:]:
+        if find_sum(preamble, number) is False:
+            return number
+        preamble.pop(0)
+        preamble.append(number)
+
+
+def find_sum(preamble, number):
+    for i in range(0, LENGTH-1):
+        for j in range(i, LENGTH):
+            if number == preamble[i] + preamble[j]:
+                return True
+    return False
+
+
+def solve2(numbers, target):
+    for i in range(0, len(numbers)-1):
+        for j in range(i+2, len(numbers)):
+            preamble = numbers[i:j]
+            if sum(preamble) == target:
+                return min(preamble) + max(preamble)
+            elif sum(preamble) > target:
+                break
+
+
+if __name__ == "__main__":
+    data = read('input.txt')
+    numbers = array.array('Q')
+    numbers.fromlist(data)
+
+    invalid_number = solve(numbers)
+
+    print(invalid_number)
+    print(solve2(numbers, invalid_number))
