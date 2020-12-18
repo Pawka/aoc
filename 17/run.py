c4d7eaa0ce70bdd8be278d2fd12432ecb4326e82
Povilas Balzaravicius Pawka <pavvka@gmail.com>
2020-12-18 15:43:36 +0200

    17: refactored

--- 17/run.py
+++ 17/run.py
@@ -1,7 +1,7 @@
 from pprint import pprint
 from collections import namedtuple
 
-Dimmensions = namedtuple('Dimmensions', 'size height width')
+Dimmensions = namedtuple('Dimmensions', 'size height width time')
 
 
 def read(name):
@@ -17,7 +17,7 @@ def solve(data):
     cube = [data]
 
     for _ in range(6):
-        dimm = Dimmensions(len(cube), len(cube[0]), len(cube[0][0]))
+        dimm = Dimmensions(len(cube), len(cube[0]), len(cube[0][0]), 1)
 
         new_cube = []
         for depth in range(0, dimm.size+1):
@@ -36,28 +36,25 @@ def solve(data):
     return total_cubes
 
 
-def get_friends(cube, row, col, depth, dimmensions):
-    size, height, width = dimmensions
+def get_friends(cube, row, col, depth, dimm):
     count = 0
-    for z in range(depth-1, 1+min(depth+1, size-1)):
+    for z in range(depth-1, 1+min(depth+1, dimm.size-1)):
         z_tocheck = z
         if z < 0:
-            if size == 1:
+            if dimm.size == 1:
                 continue
-            z_tocheck = min(1, size-1)
+            z_tocheck = min(1, dimm.size-1)
 
         inclusive = not z == depth
         count += get_flat_friends(
-                cube[z_tocheck], row, col, dimmensions, inclusive)
+                cube[z_tocheck], row, col, dimm, inclusive)
     return count
 
 
-def get_flat_friends(matrix, row, col, dimmensions, inclusive=False):
-    _, height, width = dimmensions
-
+def get_flat_friends(matrix, row, col, dimm, inclusive=False):
     count = 0
-    for i in range(max(0, row-1), 1+min(height-1, row+1)):
-        for j in range(max(0, col-1), 1+min(width-1, col+1)):
+    for i in range(max(0, row-1), 1+min(dimm.height-1, row+1)):
+        for j in range(max(0, col-1), 1+min(dimm.width-1, col+1)):
             if matrix[i][j] == 1:
                 if row == i and col == j and inclusive is False:
                     continue
@@ -71,7 +68,7 @@ def get_flat_friends(matrix, row, col, dimmensions, inclusive=False):
 ###
 """
 test_case = read('input2.txt')
-test_dimmensions = Dimmensions(1, 3, 3)
+test_dimmensions = Dimmensions(1, 3, 3, 1)
 assert get_flat_friends(test_case, 0, 0, test_dimmensions) == 1
 assert get_flat_friends(test_case, 0, 1, test_dimmensions) == 1
 assert get_flat_friends(test_case, 1, 0, test_dimmensions) == 3
@@ -86,17 +83,17 @@ assert get_friends([test_case], 0, 0, 0, test_dimmensions) == 1
 assert get_friends([test_case], 1, 1, 0, test_dimmensions) == 5
 assert get_friends([test_case], 0, 0, 1, test_dimmensions) == 1
 assert get_friends([test_case], 1, 0, 0, test_dimmensions) == 3
-assert get_friends([test_case, test_case], 1, 1, 0, Dimmensions(2, 3, 3)) == 15
-assert get_friends([test_case, test_case], 1, 1, 1, Dimmensions(2, 3, 3)) == 10
+assert get_friends(
+        [test_case, test_case], 1, 1, 0, Dimmensions(2, 3, 3, 1)) == 15
+assert get_friends(
+        [test_case, test_case], 1, 1, 1, Dimmensions(2, 3, 3, 1)) == 10
 
 
-def is_active(cube, row, col, depth, dimmensions):
-    size, height, width = dimmensions
-
-    friends = get_friends(cube, row, col, depth, dimmensions)
-
-    if (row < 0 or row >= height or col < 0 or col >= width or depth >= size or
-            cube[depth][row][col] == 0):
+def is_active(cube, row, col, depth, dimm):
+    friends = get_friends(cube, row, col, depth, dimm)
+    if (row < 0 or row >= dimm.height or col < 0 or col >= dimm.width
+            or depth >= dimm.size
+            or cube[depth][row][col] == 0):
         return friends == 3
     return (cube[depth][row][col] == 1 and friends in [2, 3])
 
