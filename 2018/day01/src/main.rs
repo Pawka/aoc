fa5f18bf4f49c947eb4156ffdc9061b3ad06e77f
Povilas Balzaravicius Pawka <povilas@balzaravicius.lt>
2021-08-18 21:47:02 +0300

    2018: day01.b

--- 2018/day01/src/main.rs
+++ 2018/day01/src/main.rs
@@ -1,8 +1,10 @@
+use std::collections::HashSet;
 use std::fs;
 
 fn main() {
     let filename = "input.txt";
     part_a(&filename);
+    part_b(&filename);
 }
 
 fn part_a(filename: &str) {
@@ -13,4 +15,21 @@ fn part_a(filename: &str) {
         .fold(0, |a, b| a + b);
 
     println!("Day01(A): {}", res);
+}
+
+fn part_b(filename: &str) {
+    let contents = fs::read_to_string(filename).expect("Can't read the file.");
+    let numbers = contents.lines().map(|l| l.parse::<i32>().unwrap()).cycle();
+
+    let mut hm: HashSet<i32> = HashSet::new();
+    let mut freq: i32 = 0;
+
+    for n in numbers {
+        freq = freq + n;
+        if hm.contains(&freq) {
+            break;
+        }
+        hm.insert(freq);
+    }
+    println!("Day01(B): {}", freq);
 }
