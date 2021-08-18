2b0194d265ba5eaf53a7b3335b9ad9348c7b425a
Povilas Balzaravicius Pawka <povilas@balzaravicius.lt>
2021-08-18 10:49:09 +0300

    2018: day01.a - simplified code

--- 2018/day01/src/main.rs
+++ 2018/day01/src/main.rs
@@ -7,20 +7,10 @@ fn main() {
 
 fn part_a(filename: &str) {
     let contents = fs::read_to_string(filename).expect("Can't read the file.");
-
-    let iter = contents.lines();
-    let mut res = 0;
-    for line in iter {
-        let prefixes: &[_] = &['-', '+'];
-        let num_str = line.trim_start_matches(prefixes);
-        let num: i32 = num_str.parse().unwrap();
-        let op = line.chars().nth(0).unwrap();
-        if op == '+' {
-            res = res + num
-        } else {
-            res = res - num
-        }
-    }
+    let res = contents
+        .lines()
+        .map(|l| l.parse::<i32>().unwrap())
+        .fold(0, |a, b| a + b);
 
     println!("Day01(A): {}", res);
 }
