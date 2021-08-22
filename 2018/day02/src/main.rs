754e6eb6d4b968e96c0a217556c8c77c2a23f17c
Povilas Balzaravicius Pawka <povilas@balzaravicius.lt>
2021-08-22 21:49:58 +0300

    2018: day02.a - reduced code size

--- 2018/day02/src/main.rs
+++ 2018/day02/src/main.rs
@@ -1,4 +1,4 @@
-use std::collections::HashMap;
+use std::collections::{HashMap, HashSet};
 use std::fs;
 
 fn main() {
@@ -8,41 +8,32 @@ fn main() {
 
 fn part_a(filename: &str) {
     let contents = fs::read_to_string(filename).expect("Can't read the file");
-    let lines = contents.lines().map(|l| {
-        let mut hm = HashMap::new();
-        for c in l.chars() {
-            let value = match hm.get(&c) {
-                Some(value) => *value + 1,
-                None => 1,
-            };
-            hm.insert(c, value);
-        }
-        hm.retain(|_, v| (*v == 3 || *v == 2));
-        hm
-    });
-
-    let mut two = 0;
-    let mut three = 0;
-
-    for hm in lines {
-        let mut added2 = false;
-        let mut added3 = false;
-        for v in hm.values() {
-            if added2 && added3 {
-                break;
+    let counts: Vec<i32> = contents
+        .lines()
+        .map(|l| {
+            let mut hm = HashMap::new();
+            for c in l.chars() {
+                let value = match hm.get(&c) {
+                    Some(value) => *value + 1,
+                    None => 1,
+                };
+                hm.insert(c, value);
             }
-
-            if !added2 && *v == 2 {
-                two += 1;
-                added2 = true;
+            let mut res = HashSet::new();
+            for v in hm.values() {
+                if res.len() == 2 {
+                    break;
+                }
+                if *v == 2 || *v == 3 {
+                    res.insert(v.clone());
+                }
             }
+            res
+        })
+        .flatten()
+        .collect();
 
-            if !added3 && *v == 3 {
-                three += 1;
-                added3 = true;
-            }
-        }
-    }
-
-    println!("Day02(A): {}", two * three);
+    let twos = counts.iter().filter(|x| **x == 2).count();
+    let threes = counts.len() - twos;
+    println!("Day02(A): {}", twos * threes);
 }
