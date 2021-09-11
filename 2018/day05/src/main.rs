d154e32c103747d5daddb7552ea7f2a073f4e636
Povilas Balzaravicius Pawka <povilas@balzaravicius.lt>
2021-09-11 23:07:02 +0300

    2018: day05.b

--- 2018/day05/src/main.rs
+++ 2018/day05/src/main.rs
@@ -7,23 +7,41 @@ const DIFF: isize = 32;
 fn main() {
     let filename = "input.txt";
     let input = fs::read_to_string(&filename).expect("Can't read file");
-    solve_a(&input);
+    println!("Day05(a): {}", solve_a(&input));
+    println!("Day05(b): {}", solve_b(&input));
 }
 
-fn solve_a(input: &String) {
+fn solve_a(input: &String) -> usize {
     let mut stack: VecDeque<isize> = VecDeque::new();
     for c in input.trim().chars() {
         let val = c as isize;
         match stack.is_empty() {
             true => stack.push_back(val),
             false => {
-                if DIFF == isize::abs(val - stack.back().unwrap()) {
-                    stack.pop_back();
-                } else {
-                    stack.push_back(val);
+                match DIFF == isize::abs(val - stack.back().unwrap()) {
+                    true => {
+                        stack.pop_back();
+                    }
+                    false => stack.push_back(val),
                 };
             }
         };
     }
-    println!("Day05(a): {}", stack.len());
+    stack.len()
+}
+
+fn solve_b(input: &String) -> usize {
+    let mut min = usize::MAX;
+    for i in 'a'..('z' as u8 + 1) as char {
+        let modified = input
+            .clone()
+            .trim()
+            .replace(i, "")
+            .replace((i as u8 - DIFF as u8) as char, "");
+        let reacted = solve_a(&modified);
+        if reacted < min {
+            min = reacted;
+        }
+    }
+    min
 }
