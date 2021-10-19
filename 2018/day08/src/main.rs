4021587f0307557080d833a9c3a86b3f9beccfcf
Povilas Balzaravicius Pawka <povilas@balzaravicius.lt>
2021-10-19 21:27:39 +0300

    Potential solution b with wrong answer

--- 2018/day08/src/main.rs
+++ 2018/day08/src/main.rs
@@ -8,13 +8,13 @@ fn main() {
         .map(|x| x.parse().unwrap())
         .collect();
     solve_a(&numbers);
+    solve_b(&numbers);
 }
 
 fn solve_a(numbers: &Vec<i32>) {
     let mut childs = Vec::new();
     let mut meta = Vec::new();
     let mut sum: i32 = 0;
-
     let mut iter = numbers.iter();
     while let Some(n) = iter.next() {
         childs.push(*n);
@@ -32,4 +32,55 @@ fn solve_a(numbers: &Vec<i32>) {
     }
 
     println!("Day08(a): {}", sum);
+}
+
+fn solve_b(numbers: &Vec<i32>) {
+    let mut childs = Vec::new();
+    let mut meta = Vec::new();
+    let mut sum: i32 = 0;
+    let mut iter = numbers.iter();
+    let mut values: Vec<Vec<i32>> = Vec::new();
+
+    let mut depth = 0;
+    while let Some(n) = iter.next() {
+        let mcount = iter.next().unwrap();
+        if *n > 0 {
+            values.push(Vec::new());
+            childs.push(*n);
+            meta.push(mcount);
+            depth += 1;
+        } else if *n == 0 {
+            let sum = iter.by_ref().take(*mcount as usize).sum::<i32>();
+            values.get_mut(depth - 1).unwrap().push(sum);
+            if let Some(n) = childs.last_mut() {
+                *n -= 1;
+            }
+        }
+
+        while let Some(0) = childs.last() {
+            let m = meta.pop().unwrap();
+            let mut s = 0;
+            for i in iter.by_ref().take(*m as usize) {
+                if *i <= values[(depth - 1) as usize].len() as i32 {
+                    s += values[(depth - 1) as usize][(*i - 1) as usize];
+                }
+            }
+            depth -= 1;
+            if depth > 0 {
+                values.get_mut(depth - 1).unwrap().push(s);
+            } else {
+                sum = s;
+            }
+            childs.pop();
+
+            if let Some(n) = childs.last_mut() {
+                *n -= 1;
+            }
+        }
+    }
+    println!("{:?}", childs);
+    println!("{:?}", meta);
+    println!("{:?}", values);
+
+    println!("Day08(b): {}", sum);
 }
