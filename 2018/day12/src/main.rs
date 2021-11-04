d8526d83a13a5a6483d6e2bb9d41addb3314eadb
Povilas Balzaravicius Pawka <povilas@balzaravicius.lt>
2021-11-04 23:10:24 +0200

    2018: Day12.b

--- 2018/day12/src/main.rs
+++ 2018/day12/src/main.rs
@@ -1,6 +1,9 @@
 use std::collections::HashMap;
 use std::fs;
 
+const TARGET_A: i32 = 20;
+const TARGET_B: i64 = 5_0000_000_000;
+
 fn main() {
     let filename = "input.txt";
     let content = fs::read_to_string(filename).expect("can't read file");
@@ -21,17 +24,19 @@ fn main() {
         })
         .collect();
 
-    solve_a(initial, &rules);
+    solve(initial, &rules, TARGET_A, TARGET_B);
 }
 
-fn solve_a(input: &str, rules: &HashMap<&str, char>) {
+fn solve(input: &str, rules: &HashMap<&str, char>, target_iter_a: i32, target_iter_b: i64) {
     let mut shift = 0;
     let mut current = input.to_string();
     shift += grow(&mut current);
-    println!("{:#?}", rules);
-    println!("{}", current);
 
-    for _ in 0..20 {
+    let mut avg = Vec::with_capacity(1000);
+    let mut last_sum = 0;
+    let mut delta = 0.0;
+    let target_it = 1000;
+    for it in 1..=100000 {
         let len = current.len();
         let mut s = String::with_capacity(len);
         s.push_str("..");
@@ -45,15 +50,38 @@ fn solve_a(input: &str, rules: &HashMap<&str, char>) {
         }
         current = s;
         shift += grow(&mut current);
-        println!("{}", current);
+        let sum = sum(&current, shift);
+        if it == target_iter_a {
+            println!("Day12(a): {}", sum);
+        }
+        if last_sum != 0 {
+            avg.push(sum - last_sum);
+        }
+        last_sum = sum;
+        if it % 500 == 0 {
+            let s: i32 = avg.iter().sum();
+            delta = f64::from(s) / avg.len() as f64;
+            if it == target_it {
+                break;
+            }
+            avg.clear();
+        }
     }
+
+    println!(
+        "Day12(b): {}",
+        (delta as i64 * (target_iter_b - target_it as i64)) + last_sum as i64
+    );
+}
+
+fn sum(input: &String, shift: i32) -> i32 {
     let mut sum = 0;
-    for (k, c) in current.chars().enumerate() {
+    for (k, c) in input.chars().enumerate() {
         if c == '#' {
             sum += k as i32 - shift;
         }
     }
-    println!("{:?}", sum);
+    sum
 }
 
 fn grow(input: &mut String) -> i32 {
