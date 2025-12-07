ba018fbed0c98117ff01f59dd9c20b57e86386c0
Povilas Balzaravičius <povilas@balzaravicius.lt>
2025-12-07 16:33:32 +0200

    2025: day04.b

--- 2025/day04/main.go
+++ 2025/day04/main.go
@@ -33,9 +33,8 @@ func main() {
 		log.Fatal(err)
 	}
 
-	data := matrix(lines)
-
-	solve(data)
+	solveA(matrix(lines))
+	solveB(matrix(lines))
 }
 
 const (
@@ -61,7 +60,7 @@ func matrix(input []string) [][]int {
 	return data
 }
 
-func solve(data [][]int) {
+func solveA(data [][]int) {
 	var (
 		width  = len(data[0])
 		height = len(data)
@@ -97,6 +96,69 @@ func solve(data [][]int) {
 				result += 1
 			}
 		}
+	}
+
+	fmt.Println(result)
+}
+
+type Roll struct {
+	x, y int
+}
+
+func solveB(data [][]int) {
+	var (
+		width  = len(data[0])
+		height = len(data)
+		rolls  = make([]Roll, 0, width*height)
+
+		result int
+	)
+
+	// Collect a list of available rolls.
+	for y, line := range data {
+		for x, _ := range line {
+			if data[y][x] == Paper {
+				rolls = append(rolls, Roll{x, y})
+			}
+		}
+	}
+
+	wasRemoved := true
+	for wasRemoved {
+		wasRemoved = false
+		removed := 0
+
+		for _, roll := range rolls {
+			x, y := roll.x, roll.y
+			if data[y][x] == Empty {
+				continue
+			}
+
+			// Check
+			pappersArround := 0
+			for x1 := x - 1; x1 <= x+1; x1++ {
+				for y1 := y - 1; y1 <= y+1; y1++ {
+					if x1 < 0 || x1 >= width {
+						continue
+					}
+					if y1 < 0 || y1 >= height {
+						continue
+					}
+					if x1 == x && y1 == y {
+						continue
+					}
+					if data[y1][x1] == Paper {
+						pappersArround += 1
+					}
+				}
+			}
+			if pappersArround < 4 {
+				data[y][x] = Empty
+				removed += 1
+				wasRemoved = true
+			}
+		}
+		result += removed
 	}
 
 	fmt.Println(result)
